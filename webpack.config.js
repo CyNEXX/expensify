6ee65f10eae705c23c2363337a8a73029839845d
CyNEXX <stefanita_rusu@yahoo.com>
2021-04-27 00:19:37 +0300

    Setup dev dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env) => {
         mode: env.production ? 'production' : 'development',
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -35,7 +35,8 @@ module.exports = (env) => {
         devtool: env.production ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/',
         }
     };
 };
